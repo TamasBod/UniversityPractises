747e61b8a517219b103e5ce9e558f55c642daa61
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-06-05 17:34:29 +0200

    Final commit

--- Functions_Controlers/Functions_Controlers/Form1.cs
+++ Functions_Controlers/Functions_Controlers/Form1.cs
@@ -15,11 +15,35 @@ namespace Functions_Controlers
         public Form1()
         {
             InitializeComponent();
+
         }
 
-        int Sum(int a, int b)
-        { 
-        
+        int Fibonacci(int n)
+        {
+            if (n == 0 || n==1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Put();
+        }
+
+        private void Put()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Button b = new Button();
+                b.Top = 40 * i;
+                b.Height = 40;
+                b.Width = 40;
+
+                b.Text = Fibonacci(i).ToString();
+
+                Controls.Add(b);
+
+            }
         }
     }
 }
