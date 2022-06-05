dbb5633abeb694a1c631e564dfb4fec96d095a82
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-06-05 15:50:05 +0200

    Final commit

--- Multiplication_table/Multiplication_table/Form1.cs
+++ Multiplication_table/Multiplication_table/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Multiplication_table
@@ -15,6 +8,26 @@ namespace Multiplication_table
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Button b = new Button();
+                    b.Height = 40;
+                    b.Width = 40;
+                    b.Left = i * 40;
+                    b.Top = j * 40;
+
+                    b.Text = (i * j).ToString();
+
+                    Controls.Add(b);
+                }
+            }
+
         }
     }
 }
