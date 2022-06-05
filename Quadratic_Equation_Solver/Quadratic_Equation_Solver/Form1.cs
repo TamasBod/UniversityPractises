3c85e6d0f973d9c4f0ccded21c33368ef428415c
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-06-05 16:38:24 +0200

    final commit

--- Quadratic_Equation_Solver/Quadratic_Equation_Solver/Form1.cs
+++ Quadratic_Equation_Solver/Quadratic_Equation_Solver/Form1.cs
@@ -15,12 +15,63 @@ namespace Quadratic_Equation_Solver
         public Form1()
         {
             InitializeComponent();
+
+
         }
 
         private void buttonstart_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtb1.Text);
 
+
+            double a;
+            if (double.TryParse(txtb1.Text, out a)==false)
+            {
+                MessageBox.Show("Incorrect input data (a)");
+                return;
+            }
+            double b;
+            if (double.TryParse(txtb2.Text, out b) == false)
+            {
+                MessageBox.Show("Incorrect input data (b)");
+                return;
+            }
+            double c;
+            if (double.TryParse(txtb3.Text, out c) ==false)
+            {
+                MessageBox.Show("Incorrect input data (c)");
+                return;
+            }
+            
+
+            double d = Math.Pow(b, 2)-4*a*c;
+
+            if (d < 0)
+            {
+                txtb4.Visible = false;
+                txtb5.Visible = false;
+                return;
+            }
+            else
+            {
+                txtb4.Visible = true;
+                txtb5.Visible = true;
+            }
+
+            double square_root = (-b + Math.Sqrt(d)) / (2 * a);
+            double square_root2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+            if (square_root == square_root2)
+            {
+                txtb5.Visible = false;
+            }
+            else
+            {
+                txtb2.Visible = true;
+            }
+
+
+            txtb4.Text = square_root.ToString();
+            txtb5.Text = square_root2.ToString();
         }
     }
 }
