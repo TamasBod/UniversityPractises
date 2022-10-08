d595734e01bca72a13a8f13c54f2b0c0be6c2956
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-10-08 14:46:09 +0200

    create new user and add to the list

--- UserMaintenance_2022/UserMaintenance_2022/Form1.Designer.cs
+++ UserMaintenance_2022/UserMaintenance_2022/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.lbllastname = new System.Windows.Forms.Label();
+            this.lblfirstname = new System.Windows.Forms.Label();
+            this.lastnametxt = new System.Windows.Forms.TextBox();
+            this.firstnametxt = new System.Windows.Forms.TextBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listUsers
+            // 
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.Location = new System.Drawing.Point(13, 13);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(314, 420);
+            this.listUsers.TabIndex = 0;
+            // 
+            // lbllastname
+            // 
+            this.lbllastname.AutoSize = true;
+            this.lbllastname.Location = new System.Drawing.Point(348, 13);
+            this.lbllastname.Name = "lbllastname";
+            this.lbllastname.Size = new System.Drawing.Size(35, 13);
+            this.lbllastname.TabIndex = 1;
+            this.lbllastname.Text = "label1";
+            // 
+            // lblfirstname
+            // 
+            this.lblfirstname.AutoSize = true;
+            this.lblfirstname.Location = new System.Drawing.Point(348, 54);
+            this.lblfirstname.Name = "lblfirstname";
+            this.lblfirstname.Size = new System.Drawing.Size(35, 13);
+            this.lblfirstname.TabIndex = 2;
+            this.lblfirstname.Text = "label2";
+            // 
+            // lastnametxt
+            // 
+            this.lastnametxt.Location = new System.Drawing.Point(448, 13);
+            this.lastnametxt.Name = "lastnametxt";
+            this.lastnametxt.Size = new System.Drawing.Size(100, 20);
+            this.lastnametxt.TabIndex = 3;
+            // 
+            // firstnametxt
+            // 
+            this.firstnametxt.Location = new System.Drawing.Point(448, 54);
+            this.firstnametxt.Name = "firstnametxt";
+            this.firstnametxt.Size = new System.Drawing.Size(100, 20);
+            this.firstnametxt.TabIndex = 4;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(333, 382);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(206, 51);
+            this.AddBtn.TabIndex = 5;
+            this.AddBtn.Text = "button1";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.firstnametxt);
+            this.Controls.Add(this.lastnametxt);
+            this.Controls.Add(this.lblfirstname);
+            this.Controls.Add(this.lbllastname);
+            this.Controls.Add(this.listUsers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Label lbllastname;
+        private System.Windows.Forms.Label lblfirstname;
+        private System.Windows.Forms.TextBox lastnametxt;
+        private System.Windows.Forms.TextBox firstnametxt;
+        private System.Windows.Forms.Button AddBtn;
     }
 }
 
--- UserMaintenance_2022/UserMaintenance_2022/Form1.cs
+++ UserMaintenance_2022/UserMaintenance_2022/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance_2022.Entities;
 
 namespace UserMaintenance_2022
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            lbllastname.Text = Resource1.LastName;
+            lblfirstname.Text = Resource1.FirstName;
+            AddBtn.Text = Resource1.Add;
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+
+            var u = new User()
+            {
+                LastName = lastnametxt.Text,
+                FirstName = firstnametxt.Text
+            };
+            users.Add(u);
+
         }
     }
 }
