553dd8fb15534ebe083e40d93502cd10d27190f6
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-11-14 06:56:01 +0100

    Task 4.: Load ramen objects

--- LINQ_Practise/LINQ_Practise/Form1.cs
+++ LINQ_Practise/LINQ_Practise/Form1.cs
@@ -6,36 +6,46 @@ namespace LINQ_Practise
     public partial class Form1 : Form
     {
         private List<Country> _countries = new List<Country>();
+        private List<Ramen> _ramens = new List<Ramen>();
         public Form1()
         {
             InitializeComponent();
             Loaddata("ramen.csv");
+            AddCountry("ramen.csv");
         }
 
         private void Loaddata(string filename)
         {
             using (StreamReader sr = new StreamReader(filename, Encoding.Default))
             {
-                sr.ReadLine(); // Header
+                sr.ReadLine();
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
 
                     var countryName = line[2];
-                    var currentCountry = (from c in _countries
-                                          where c.Name.Equals(countryName)
-                                          select c).FirstOrDefault();
-                    if (currentCountry == null)
-                    {
-                        currentCountry = new Country()
-                        {
-                            ID = _countries.Count + 1,
-                            Name = countryName
-                        };
-                        _countries.Add(currentCountry);
-                    }
+                    AddCountry(countryName);
                 }
             }
         }
+
+
+        private void AddCountry(string countryName)
+        {
+            var currentCountry = (from c in _countries
+                                  where c.Name.Equals(countryName)
+                                  select c).FirstOrDefault();
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID = _countries.Count + 1,
+                    Name = countryName
+                };
+                _countries.Add(currentCountry);
+            }
+        }
+
+       
     }
 }
