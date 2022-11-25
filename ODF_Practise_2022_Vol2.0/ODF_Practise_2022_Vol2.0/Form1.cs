8fcf862f23c2ecac5651e16e5c78ea705d69c94e
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-11-25 16:02:36 +0100

    Task 4.

--- ODF_Practise_2022_Vol2.0/ODF_Practise_2022_Vol2.0/Form1.cs
+++ ODF_Practise_2022_Vol2.0/ODF_Practise_2022_Vol2.0/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace ODF_Practise_2022_Vol2._0
 {
+    
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
