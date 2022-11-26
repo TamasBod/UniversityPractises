0e1acc0e8f98d5c65d4a8c6d46690abeae99ba1b
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-11-26 09:25:24 +0100

    Task 4: Árfolyam lista létrehozása

--- XML_Practise_2022/XML_Practise_2022/Form1.Designer.cs
+++ XML_Practise_2022/XML_Practise_2022/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGW1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGW1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGW1
+            // 
+            this.dataGW1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGW1.Location = new System.Drawing.Point(123, 117);
+            this.dataGW1.Name = "dataGW1";
+            this.dataGW1.Size = new System.Drawing.Size(240, 150);
+            this.dataGW1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGW1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGW1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGW1;
     }
 }
 
--- XML_Practise_2022/XML_Practise_2022/Form1.cs
+++ XML_Practise_2022/XML_Practise_2022/Form1.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XML_Practise_2022.Entities;
 using XML_Practise_2022.MnbServiceReference;
 
 namespace XML_Practise_2022
 {
     public partial class Form1 : Form
     {
-
-
+        BindingList<RateData> rates = new BindingList<RateData>();
 
         public Form1()
         {
             InitializeComponent();
+            getRates();
+            dataGW1.DataSource = rates;
 
+        }
+
+        private void getRates()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
-
             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = "EUR",
@@ -30,8 +36,8 @@ namespace XML_Practise_2022
             };
 
             var response = mnbService.GetExchangeRates(request);
-
             var result = response.GetExchangeRatesResult;
+            //File.WriteAllText("Teszt.xml", result);
         }
     }
 }
