1b57bd87823a0977a15cc0c726a69731d08a28bc
Tamás Gábor Bodor <tamas.bodor@stud.uni-corvinus.hu>
2022-12-14 14:11:05 +0100

    Task 7: Value of portfolio

--- VAR_2022/VAR_2022/Form1.cs
+++ VAR_2022/VAR_2022/Form1.cs
@@ -14,8 +14,8 @@ namespace VAR_2022
     public partial class Form1 : Form
     {
         PortfolioEntities context = new PortfolioEntities();
-        List<Tick> Ticks
-        List<PortfolioItem> Portfolio;
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
@@ -23,7 +23,6 @@ namespace VAR_2022
             Ticks = context.Ticks.ToList();
             dGw.DataSource = Ticks;
             CreatePortfolio();
-
 
         }
 
@@ -34,6 +33,21 @@ namespace VAR_2022
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dGW2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
